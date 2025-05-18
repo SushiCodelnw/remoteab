d4095b02fbf48f74e3ab90679fee06fa33d031f8
SushiCodelnw <1389Dead@gmail.com>
2025-05-18 16:56:41 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,58 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue("Arm", control.deviceSerialNumber())
+    Set = 1
+    basic.showIcon(IconNames.Square)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendValue("Hand", control.deviceSerialNumber())
+    Set = 0
+    basic.showIcon(IconNames.Triangle)
 })
+let lastCommand = ""
+let command = ""
 let Y = 0
 let X = 0
+let Set = 0
 radio.setGroup(1)
 basic.forever(function () {
     X = input.rotation(Rotation.Roll)
     Y = input.rotation(Rotation.Pitch)
-    if (Y < -30) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
-        radio.sendValue("Forward", control.deviceSerialNumber())
-    } else if (Y > 30) {
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
-        radio.sendValue("Forback", control.deviceSerialNumber())
-    } else if (X < -30) {
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-        radio.sendValue("Left", control.deviceSerialNumber())
-    } else if (X > 30) {
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
-        radio.sendValue("Right", control.deviceSerialNumber())
+    if (Set == 0) {
+        if (Y < -30) {
+            command = "Forward"
+            basic.showArrow(ArrowNames.North)
+        } else if (Y > 30) {
+            command = "Forback"
+            basic.showArrow(ArrowNames.South)
+        } else if (X < -30) {
+            command = "Left"
+            basic.showArrow(ArrowNames.West)
+        } else if (X > 30) {
+            command = "Right"
+            basic.showArrow(ArrowNames.East)
+        } else {
+            command = "Stop"
+            basic.clearScreen()
+        }
     } else {
-        basic.clearScreen()
+        if (Y < -30) {
+            command = "ArmUp"
+            basic.showArrow(ArrowNames.North)
+        } else if (Y > 30) {
+            command = "ArmDown"
+            basic.showArrow(ArrowNames.South)
+        } else if (X < -30) {
+            command = "HandUp"
+            basic.showArrow(ArrowNames.West)
+        } else if (X > 30) {
+            command = "HandDown"
+            basic.showArrow(ArrowNames.East)
+        } else {
+            command = "Stop"
+            basic.clearScreen()
+        }
     }
+    if (command != lastCommand) {
+        radio.sendValue(command, control.deviceSerialNumber())
+        lastCommand = command
+    }
+    basic.pause(100)
 })
